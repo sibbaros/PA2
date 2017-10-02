de339781d0e66e005316ad49ee603f9faea7e99f
alexandrag16 <alexandrag16@skel.ru.is>
2017-10-02 22:04:51 +0000

    includes

--- src/httpd.c
+++ src/httpd.c
@@ -1,6 +1,17 @@
 /* your code goes here. */
+#include <assert.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	int sockfd;
+	struct sockaddr_in server, client;
+	
 	return 0;
 }
