58daf16c468ae9e79070cfb0c9c2e586eafbfd94
sigurbjorg15 <sigurbjorg15@skel.ru.is>
2017-10-03 13:51:14 +0000

    make virkar

--- src/httpd.c
+++ src/httpd.c
@@ -11,30 +11,30 @@
 #include <stdbool.h>
 
 
-void head() {
+//void head() {
      // returns the header of the page ( doesn't have to be a in it's own function can be) 
-}
+//}
 
-void get() {
+//void get() {
     // generates a HTML5 page in memmory ( think it should be in a seperate function)
     // Itsactual content should include the URL of the requested page and the IP address and port number of the requesting client
     // format http://foo.com/page 123.123.123.123:4567
-}
+//}
 
-void post() {
+//void post() {
    // same as get request plus the data in the body of the post request
-}
+//}
 
-void error() {
+//void error() {
    // sends an error msg
-}
+//}
 
 int main(int argc, char *argv[]) {
 	int sockfd;
 	struct sockaddr_in server, client;
 
-	for(int i = 0; i < sizeOf(argv); i++) {
-		printf("%d", argv[i]);
+	for(int i = 0; i < 10; i++) {
+		printf("%d \n", argv[i]);
 	}
         // close connections if no activity in 30 sec
         // client can send a "connection: closed" msg
