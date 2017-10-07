41c5727ab5d273310822842ade2dfe635b1f9118
Alexandra Geirsdóttir <alexandrageirs@Alexandras-MacBook-Air.local>
2017-10-07 17:53:03 +0000

    even more fixes

--- src/httpd.c
+++ src/httpd.c
@@ -20,9 +20,10 @@ void get(char *html, char *ipAddr, char *hostPort, char *hostIP) {
     // generates a HTML5 page in memmory ( think it should be in a seperate function)
     // Itsactual content should include the URL of the requested page and the IP address and port number of the requesting client
     // format http://foo.com/page 123.123.123.123:4567
-    html[0] = "\0";
+    html[0] = '\0';
     strcat(html, "HTTP/1.1 200, OK\nContent-type: text/html\n"
-    "\n<!DOCTYPE>\n<html>\n    <body>\n        <h1>\n"); //FKOFM8</h1></body></html>\n";
+    "\n<!DOCTYPE>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n"
+    "    </head>\n    <body>\n        <h1>\n");
     strcat(html, "            http://");
     strcat(html, ipAddr);
     strcat(html, " ");
@@ -34,30 +35,29 @@ void get(char *html, char *ipAddr, char *hostPort, char *hostIP) {
     printf("inside get function\r\n");
 }
 
-void post(char *html, char *ipAddr, char *hostPort, char *hostIP) {
+void post(char *html, char *ipAddr, char *hostPort, char *hostIP, char *data) {
    // same as get request plus the data in the body of the post request
-
-    char data;
-    html[0] = "\0";
+    html[0] = '\0';
     strcat(html, "HTTP/1.1 200, OK\nContent-type: text/html\n"
-    "\n<!DOCTYPE>\n<html>\n    <body>\n");
-    strcat(html, data);
-    strcat(html, "       <h1>\n"); //FKOFM8</h1></body></html>\n";
+    "\n<!DOCTYPE>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n"
+    "    </head>\n    <body>\n       <h1>\n");
     strcat(html, "            http://");
     strcat(html, ipAddr);
     strcat(html, " ");
     strcat(html, hostIP);
     strcat(html, ":");
     strcat(html, hostPort);
-    strcat(html, "\n        </h1>\n"
-    "<img src=\"https://http.cat/200\" alt=\"Mountain View\" style=\"width:304px;height:228px;\">    </body>\n</html>\n");
+    strcat(html, "\n        </h1>\n");
+    strcat(html, data);
+    strcat(html, "<img src=\"https://http.cat/200\" alt=\"Mountain View\" style=\"width:304px;height:228px;\">    </body>\n</html>\n");
     
 }
 
 void ifError(char *html) {
-    html[0] = "\0";
+    html[0] = '\0';
     strcat(html, "HTTP/1.1 404, NOTOK\n"
-    "<!DOCTYPE html>\n<html>\n    <body>\n       <h2>~~OOPS something went wrong~~       </h2>"
+    "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n"
+    "    </head>\n    <body>\n       <h2>~~OOPS something went wrong~~       </h2>"
     "            \"<img src=\"https://blog.hubspot.com/hs-fs/hubfs/tinsanity-404"
     "-page.gif?t=1507329261965&width=495&name=tinsanity-404-page.gif\""
     " alt=\"BAD REQUEST\" style=\"width:304px;height:228px;\">\n    </body>\n</html>");
@@ -88,12 +88,11 @@ int main(int argc, char *argv[]) {
         socklen_t len = (socklen_t) sizeof(client);
         int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
         
-        //printf("connfd: %d", connfd);
         if(connfd == 0) {
             perror("Connection failed...\n");
         }
        
-        //Get all info
+        //Get all info that we need from the client
         char hostIP[500], hostPort[32], ipAddr[INET_ADDRSTRLEN];
         struct sockaddr_in * clientSockAddr = (struct sockaddr_in*)&client;
         struct in_addr clientIP = clientSockAddr->sin_addr;
@@ -102,14 +101,12 @@ int main(int argc, char *argv[]) {
 
         //Recieve from connfd, not sockfd
         ssize_t n = recv(connfd, &message, sizeof(message) - 1, 0);
-	char html[500]; // "HTTP/1.1 200, OK\r\n\r\n<!DOCTYPE><html><body><h1>Hallu</h1></body></html>\r\n";
-	int n2 = send(connfd, &html, sizeof(html) - 1, 0);
+	    char html[500];
+	    int n2 = send(connfd, &html, sizeof(html) - 1, 0);
 
         // need to check the first message and see if it is get, post or head
         // and then send it to the right function and send it the webpage it's asking for 
         //
-	//printf("%d\n", n2);
-        //printf("the message is: %s", message);
         
         char mtype[5];
         memcpy(mtype, &message[0], 4);
@@ -121,7 +118,9 @@ int main(int argc, char *argv[]) {
         }
         else if(!(strcmp(mtype, "POST"))) {
             printf("Post request\n");
-            post(html, ipAddr, hostPort, hostIP);
+            char data[500];
+            memcpy(data, &message[5], 30);
+            post(html, ipAddr, hostPort, hostIP, data);
         }
         else if(!(strcmp(mtype, "HEAD"))) {
             printf("Head request\n");
@@ -132,10 +131,7 @@ int main(int argc, char *argv[]) {
             ifError(html);
         }
         send(connfd, &html, sizeof(html) -1, 0);
-        printf("LOL HI\n");
     }
-    
     return 0;
-
 }
 
